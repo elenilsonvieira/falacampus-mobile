9759ea266fd6e521b8b92d8a223125e3259b346b
guilhermemoraes1 <guilherme.morais.54540218@gmail.com>
2025-08-03 20:04:25 -0300

    adicionei o menu lateral

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,88 +1,94 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
+// Importação das telas
+import HomeScreen from '../(tabs)/index';
+import FeedbackScreen from '../(tabs)/feedback';
+import ExploreScreen from '../(tabs)/explore';
+import RespostasScreen from '../(tabs)/respostas';
+import DepsScreen from '../(tabs)/deps';
+import LoginScreen from '../(tabs)/login';
+import UserScreen from '../(tabs)/user';
+import FeedScreen from '../(tabs)/feed';
+
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
+    <Drawer.Navigator
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
+        headerShown: true,
+        drawerActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        drawerStyle: {
+          backgroundColor: '#fff',
+          width: 250,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="home.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="home.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="feedback"
+      <Drawer.Screen
+        name="Comentários"
+        component={FeedbackScreen}
         options={{
-          title: 'Comentários',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="send.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="send.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
+      <Drawer.Screen
+        name="Buscar Comentários"
+        component={ExploreScreen}
         options={{
-          title: 'Meus Comentários',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="mycomments.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="mycomments.fill" color={color} />,
         }}
       />
-       <Tabs.Screen
-        name="respostas"
+      <Drawer.Screen
+        name="Respostas"
+        component={RespostasScreen}
         options={{
-          title: 'Respostas',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="reply.bubble.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="reply.bubble.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="deps"
+      <Drawer.Screen
+        name="Departamentos"
+        component={DepsScreen}
         options={{
-          title: 'Departamentos',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="apartment.building.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="apartment.building.fill" color={color} />,
         }}
       />
-       <Tabs.Screen
-        name="login"
+      <Drawer.Screen
+        name="Login"
+        component={LoginScreen}
         options={{
-          title: 'Login',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="login.square.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="login.square.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="user"
+      <Drawer.Screen
+        name="Perfil"
+        component={UserScreen}
         options={{
-          title: 'Perfil',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="profile.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="profile.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="feed"
+      <Drawer.Screen
+        name="Feed"
+        component={FeedScreen}
         options={{
-          title: 'Feed',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="folder.fill" color={color} />,
+          drawerIcon: ({ color }) => <IconSymbol size={24} name="folder.fill" color={color} />,
         }}
       />
-    </Tabs>
-    
+    </Drawer.Navigator>
   );
 }
